9fa504be0973cfda4094f4b7247acaa1c28682d1
Полуночный Джун <155324952+Bl1nke@users.noreply.github.com>
2025-08-13 11:52:31 +0300

    config.py

--- exchange tg bot(using advanced knowledge)/config.py
+++ exchange tg bot(using advanced knowledge)/config.py
@@ -1,12 +1,13 @@
 import telebot
 
-TOKEN = "8311820652:AAEOOKLGqpCDJiJ0SPZDf0KyX2dBEAnrPH4"
+TOKEN = "8311820652:******Df0KyX2dBEAnrPH4"
 
-API_KEY_CRYPTO = 'CG-BsZfKwM3koaTdrzdZAjxrEVg'
+API_KEY_CRYPTO = '*******zdZAjxrEVg'
 
 URL_CBR = "https://www.cbr-xml-daily.ru/daily_json.js"
 
 URL_CRYPTO = 'https://api.coingecko.com/api/v3/simple/price'
 
 CRYPTO_CURRENCIES = ["bitcoin", "ethereum", "tether"]
+
 FIAT_CURRENCIES = ["USD", "EUR", "GBP", "AED", "CNY", "PLN", "TRY"]
